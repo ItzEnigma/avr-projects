70db016b9c7b356df212d27ac984977ace8d29a5
TaqiEldeen <takeycoo@gmail.com>
2022-12-26 17:59:59 +0200

    Add Smart Home project

---
+++ Smart Home/main.c
@@ -0,0 +1,18 @@
+/*
+ * Smart Home.c
+ *
+ * Created: 26/12/2022 05:58:27 PM
+ * Author : 20109
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
